93a6ba662a2931a70def2ae7188052bb6cff1e3e
nothingprogram <dev@jongyun.com>
2021-11-11 22:51:36 +0900

    Type unions in state

--- src/state/GeustList.tsx
+++ src/state/GeustList.tsx
@@ -2,11 +2,22 @@ import { useState } from "react";
 
 const GuestList: React.FC = () => {
     const [name, setName] = useState("");
+    const [guests, setGuests] = useState<string[]>([]);
+
+    const onClick = () => {
+        setName("");
+        setGuests([...guests, name]);
+    };
     return (
         <div>
             <h3>Guest List</h3>
+            <ul>
+                {guests.map((guest: string) => (
+                    <li key={guest}>{guest}</li>
+                ))}
+            </ul>
             <input value={name} onChange={(e) => setName(e.target.value)} />
-            <button>Add Guest</button>
+            <button onClick={onClick}>Add Guest</button>
         </div>
     );
 };

490db42fe6aa67d67b2c218fe39b9a084c481ba6
nothingprogram <dev@jongyun.com>
2021-11-13 14:30:10 +0900

    Adding Typings to Dispatch

--- redux-ts/src/state/action-creators/index.ts
+++ redux-ts/src/state/action-creators/index.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { Dispatch } from "redux";
 import { ActionType } from "../action-type";
 import { Action } from "../actions";
 
 export const searchRepositories = (term: string) => {
-  return async (dispatch: any) => {
+  return async (dispatch: Dispatch<Action>) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES,
     });
@@ -28,7 +29,7 @@ export const searchRepositories = (term: string) => {
       });
     } catch (error: any) {
       dispatch({
-        tpye: ActionType.SEARCH_REPOSITORIES_ERROR,
+        type: ActionType.SEARCH_REPOSITORIES_ERROR,
         payload: error.message,
       });
     }

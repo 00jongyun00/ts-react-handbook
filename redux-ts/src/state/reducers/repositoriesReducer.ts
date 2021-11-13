956f5d5cd06c2f821eb6563e26fd7ca1bf128709
nothingprogram <dev@jongyun.com>
2021-11-13 13:40:09 +0900

    Applying Action interfaces

--- redux-ts/src/state/reducers/repositoriesReducer.ts
+++ redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -4,19 +4,37 @@ interface RepositoriesState {
   data: string[];
 }
 
-interface Action {
-  type: string;
-  payload?: any;
+interface SearchRepositoriesAction {
+  type: "search_repositories";
+}
+
+interface SearchRepositoriesSuccessAction {
+  type: "search_repositories_success";
+  payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+  type: "search_repositories_error";
+  payload: string;
 }
 
 const reducer = (
   state: RepositoriesState,
-  action: Action
+  action:
+    | SearchRepositoriesAction
+    | SearchRepositoriesSuccessAction
+    | SearchRepositoriesErrorAction
 ): RepositoriesState => {
+  if (action.type === "search_repositories_success") {
+    // 100% certainty that 'action' satsifies the
+    // SearchRepositoriesSuccessAction interface
+    // typeof action.payload === Array<string> = true
+  }
   switch (action.type) {
     case "search_repositories":
       return { loading: true, error: null, data: [] };
     case "search_repositories_success":
+      // 100% certainty that 'action' is SearchRepositoriesSuccessAction
       return { loading: false, error: null, data: action.payload };
     case "search_repositories_error":
       return { loading: false, error: action.payload, data: [] };
